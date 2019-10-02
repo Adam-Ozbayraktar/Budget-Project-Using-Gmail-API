46686233ec6bf5793b961bcc298174defbfb4ea2
AdamOz <adamozbayraktar@live.com>
2019-10-02 11:31:04 +0200

    Inital Commit

--- OLD/time_testing.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-t = datetime.now()
-t_str = t.strftime("%d-%m-%Y")
-#print(f"{t.day}-{t.month}-{t.year}")
-print(f"The date is: {t_str}")
-print(t.strftime("%d-%m-%Y"))
--- src/OLD/testing_docs.py
+++
@@ -1,6 +0,0 @@
-from pdf_handler import convert_to_text
-from better_txt import better_txt_converter
-
-file = r'C:\Users\Study\Documents\budget_project\cache\statement-20-197-507-6 (1).pdf'
-text_file = convert_to_text(file)
-#better_text_file = better_txt_converter(text_file)
